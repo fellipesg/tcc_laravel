2a71ef9d37a6ed56482f2e87b033279bf788c9d9
Felipe Gonçalves <fgoncalves2008@gmail.com>
2019-03-28 20:52:47 -0400

    Relacionamentos

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,11 @@ class Course extends Model
     protected $fillable = ['nome', 'identificador'];
     protected $guarded = ['id','created_at', 'updated_at'];
     protected $table = 'courses';
-    //
+     /**
+     * Get the Institution that owns the course.
+     */
+    public function institution()
+    {
+        return $this->belongsTo('App\Institution');
+    }
 }
--- app/Institution.php
+++ app/Institution.php
@@ -7,4 +7,11 @@ class Institution extends Model
     protected $fillable = ['nome', 'identificador'];
     protected $guarded = ['id','created_at', 'updated_at'];
     protected $table = 'institutions';
+     /**
+     * Get the courses for the institution.
+     */
+    public function courses()
+    {
+        return $this->hasMany('App\Course');
+    }
 }
